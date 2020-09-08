fdbe10a65669ab09c3c5210c759e77cd32b34846
Borto <gbortolote@gmail.com>
2020-09-08 11:12:31 -0300

    Implementa trabalho que gera nota 7.8

--- la-lexico/src/main/antlr4/br/ufscar/dc/compiladores/al/lexico/AlLexer.g4
+++ la-lexico/src/main/antlr4/br/ufscar/dc/compiladores/al/lexico/AlLexer.g4
@@ -1,33 +1,52 @@
 lexer grammar AlLexer;
 
+/* 
+*    Palavras chave serão convertidas em tokens do tipo: <'key', 'key'>
+*/
 PALAVRA_CHAVE: 'algoritmo' | 'fim_algoritmo' | 
-               'var' | 'declare' | 'escreva' | ':' | 
-               'funcao' | 'fim_funcao' | 'procedimento' | 'fim_procedimento' |
-               'literal' | 'inteiro' | 'real' | 'logico' | 'constante' |
-               'verdadeiro' | 'falso' |
-               'registro' | 'fim_registro' |
-               'tipo' | 
-               'leia' | 'escreva' | 'se' | 'entao' | 'senao' | 'fim_se' |
-               'caso' | 'seja' | 'fim_caso' | 'retorne' |
-               'para' | 'ate' | 'faca' | 'fim_para' | 'ate' |
-               '+' | '-' | '*' | '%' | '/' | '&' | 'ou' | 'e' | 
+               'declare' | 'constante' | 'tipo' |
+               'literal' | 'inteiro' | 'real' | 'logico' |
+               'verdadeiro' | 'falso' | 'registro' | 'fim_registro' |
+               'procedimento' | 'fim_procedimento' | 'funcao' | 'fim_funcao' |
+               'var' | 'leia' | 'escreva' | 'se' | 'entao' | 'senao' | 'fim_se' | 
+               'caso' | 'seja' | 'fim_caso' | 'para' | '<-' | 'ate' | 'faca' | 'fim_para' |
+               'enquanto' | 'fim_enquanto' | 'retorne' | 'nao' | '..' | ':' |
+               '^' | '+' | '-' | '*' | '%' | '/' | '&' | 'ou' | 'e' | 
                '>' | '>=' | '<' | '<=' | '<>' | '=' |
-               '(' | ')' | ',' | '.' | '[' | ']' |
-               '<-' | '->' ;
+               '(' | ')' | ',' | '.' | '[' | ']';
 
-NUM_INT: ('0'..'9')+;
-
-NUM_REAL: ('0'..'9')+ ('.' ('0'..'9')+)?;
-
-
+/* 
+*    Todo identificador é formado apenas por letras do alfabeto (a a z), por dígitos (0 a 9) e pela sublinha (_);
+*    O primeiro caractere é sempre uma letra ou a sublinha; nunca um dígito;
+*    O identificador escolhido deve ser significativo. 
+*/
 IDENT: ('a'..'z'|'A'..'Z'|'_') ('a'..'z'|'A'..'Z'|'0'..'9'|'_')*;
 
-CADEIA: '"' (ESC_SEQ | ~('"' |'\\') )* '"';
-
-
+/* 
+*    Toda string começa e termina com aspas e pode ter qualquer coisa que não seja
+*    aspas, a menos que esta tenho um scape antecedente
+*/
 fragment
 ESC_SEQ: '\\"';
+CADEIA: '"' (ESC_SEQ | ~('"') )* '"';
 
-COMENTARIO: '{' ~('\n' | '\r')* '}' -> skip;
+/*
+*    Comentários não geram tokens e são limitados por chaves
+*/
+COMENTARIO: '{' ~('\n' | '\r' | '}')* '}' -> skip; 
 
+
+/*
+*     Números inteiros são qualquer sequência de um ou mais digitos
+*/
+NUM_INT: ('0'..'9')+;
+
+/*
+*     Números reais são qualquer sequência de um ou mais digitos com um ponto no meio
+*/
+NUM_REAL: ('0'..'9')+ ('.' ('0'..'9')+)?;
+
+/*
+*     Evita erros ao encontrar espaços, tabs e quebras de linha
+*/
 WS: ( ' ' | '\t' | '\r' | '\n') {skip();};
--- la-lexico/src/main/java/br/ufscar/dc/compiladores/al/lexico/Principal.java
+++ la-lexico/src/main/java/br/ufscar/dc/compiladores/al/lexico/Principal.java
@@ -1,13 +1,14 @@
 package br.ufscar.dc.compiladores.al.lexico;
 
 
-import br.ufscar.dc.compiladores.al.lexico.AlLexer;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import org.antlr.v4.runtime.CharStream;
 import org.antlr.v4.runtime.CharStreams;
 import org.antlr.v4.runtime.Token;
+import org.antlr.v4.runtime.misc.ParseCancellationException;
 
 
 public class Principal {
@@ -17,18 +18,36 @@ public class Principal {
             cs = CharStreams.fromFileName(args[0]);
             AlLexer lex = new AlLexer(cs);
             
-            Token t = null;
-            while ((t = lex.nextToken()).getType() != Token.EOF) {
-                if("PALAVRA_CHAVE" == AlLexer.VOCABULARY.getDisplayName(t.getType()))
-                    System.out.println("<'" + t.getText() + "','" + t.getText() + "'>");
-                else 
-                    System.out.println("<'" + t.getText() + "'," + AlLexer.VOCABULARY.getDisplayName(t.getType()) + ">");
-            }
+            // remove os manipuladores de erros padrão da classe Lexer da ANTRL
+            lex.removeErrorListeners();
             
-        } catch (IOException ex) {
-            Logger.getLogger(Principal.class.getName()).log(Level.SEVERE, null, ex);
+            // adicione o manipulador que criamos para lançar a excessão ParseCancellationException
+            lex.addErrorListener(ErrorListener.INSTANCE);
+            
+            FileWriter myWriter = new FileWriter(args[1]);
+            Token t = null;
+            
+            try {
+                while ((t = lex.nextToken()).getType() != Token.EOF) {
+                    if("PALAVRA_CHAVE".equals(AlLexer.VOCABULARY.getDisplayName(t.getType()))){
+                        myWriter.write("<'" + t.getText() + "','" + t.getText() + "'>\n");
+                        System.out.println("<'" + t.getText() + "','" + t.getText() + "'> printed");
+                    }
+                    else{
+                        myWriter.write("<'" + t.getText() + "'," + AlLexer.VOCABULARY.getDisplayName(t.getType()) + ">\n");
+                        System.out.println("<'" + t.getText() + "'," + AlLexer.VOCABULARY.getDisplayName(t.getType()) + "> printed");
+                    }
+                }
+            } catch (ParseCancellationException e) {
+                myWriter.write(e.getMessage());
+                System.out.println(e.getMessage() + "printed\nExecution stoped");
+            } finally{
+                myWriter.close();
+            }
         }
         
-                
+        catch (IOException ex) {
+            Logger.getLogger(Principal.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
 }
